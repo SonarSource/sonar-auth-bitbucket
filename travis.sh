012533b7e557c05abda877b8f862998a42dc1650
Tom <thomas.verin@sonarsource.com>
2018-05-02 11:03:08 +0200

    BUILD: setup pipeline

---
+++ travis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function installTravisTools {
+  mkdir -p ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v48 | tar zx --strip-components 1 -C ~/.local
+  source ~/.local/bin/install
+}
+
+installTravisTools
+source ~/.local/bin/installMaven35
+
+export DEPLOY_PULL_REQUEST=true
+regular_mvn_build_deploy_analyze
