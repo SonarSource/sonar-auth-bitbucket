5ef820f097c86da4b2c339db237f26c0542c8008
Simon Brandhof <simon.brandhof@sonarsource.com>
2020-03-20 09:57:11 +0100

    Fix build promotion after drop of Jenkins

--- travis.sh
+++ travis.sh
@@ -4,14 +4,16 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v57 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v58 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
+  source ~/.local/bin/setup_promote_environment
 }
 
 installTravisTools
 source ~/.local/bin/installMaven35
 
 export DEPLOY_PULL_REQUEST=true
-regular_mvn_build_deploy_analyze
+. regular_mvn_build_deploy_analyze
+promote
 
 ./check-license-compliance.sh
